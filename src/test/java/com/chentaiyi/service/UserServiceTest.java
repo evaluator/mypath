3318027a41c849fe0487b75a4b78714eeb5a0476
taiyi <evalulate@163.com>
2017-10-23 21:33:01 +0800

    增加单元测试

---
+++ src/test/java/com/chentaiyi/service/UserServiceTest.java
@@ -0,0 +1,18 @@
+package com.chentaiyi.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+
+/**
+ * Created by hasee on 2017/10/23.
+ */
+public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void hasMatchUser(){
+
+    }
+}
