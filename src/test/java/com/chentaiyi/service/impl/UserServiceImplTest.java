3beed8b61503578ac427c1b1e8cb7aff51e2524f
taiyi <evalulate@163.com>
2017-10-23 21:08:06 +0800

    增加单元测试

---
+++ src/test/java/com/chentaiyi/service/impl/UserServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.chentaiyi.service.impl;
+
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created by hasee on 2017/10/23.
+ */
+@ContextConfiguration("classpath*:/mypath-context.xml")
+public class UserServiceImplTest {
+}
